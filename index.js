0af6e778705af5b9e0d7fd69395f5e57a675c9b8
Subhransu26 <subhransusahu95@gmail.com>
2025-06-08 23:13:36 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const app = express();
+
+const path = require("path");
+
+const fs = require("fs");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.set("view engine", "ejs");
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+  fs.readdir(`./files`, (err, files) => {
+    res.render("index", { files: files });
+  });
+});
+
+app.post("/create", (req, res) => {
+  fs.writeFile(
+    `./files/${req.body.title.split(" ").join("")}.txt`,
+    req.body.details,
+    () => {
+      res.redirect("/");
+    }
+  );
+});
+
+app.get("/file/:filename", (req, res) => {
+  fs.readFile(`./files/${req.params.filename}`, "utf-8", (err, filedata) => {
+    res.render("show", { title: req.params.filename, details: filedata });
+  });
+});
+
+app.listen(3000, () => {
+  console.log("server running");
+});
